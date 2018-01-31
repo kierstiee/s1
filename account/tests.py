37089d3a3760fe382621454149b80c62ffc54355
kierstiee <kierstiee@gmail.com>
2018-01-31 15:15:59 -0700

    all but logout - not needed

--- account/tests.py
+++ account/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Group, Permission, ContentType
+from django.contrib.auth import authenticate, login, logout
 from account import models as amod
 
 
@@ -19,7 +19,6 @@ class UserClassTestCase(TestCase):
         self.u1.zipcode = '12345'
         self.u1.save()
 
-
     def test_load_save(self):
         """Test creating, saving, and reloading a user"""
         u2 = amod.User.objects.get(email = 'lisa@simpsons.com') # or could say 'email = u1.email'
@@ -34,41 +33,62 @@ class UserClassTestCase(TestCase):
 
     def test_adding_group_perm(self):
         """Test adding a group w/ permissions"""
-        new_group, created = Group.objects.get_or_create(name = 'test_group')
-        contenttype = ContentType.objects.get_for_model(amod.User)
-        can_view = Permission.objects.create(codename='can_view', name = 'Can View', content_type = contenttype)
-        new_group.permissions.add(can_view)
+        new_group = Group(name = 'test_group')
+        new_group.save()
+
+        account = ContentType.objects.get_for_model(amod.User)
+        permission = Permission(codename='change_product_price',
+                                name = 'Change Price of a Product',
+                                content_type = account)
+        permission.save()
+        new_group.permissions.add(permission)
+        new_group.save()
+
         self.u1.groups.add(new_group)
         self.u1.save()
 
+        test = self.u1.has_perm('account.change_product_price')
+        self.assertTrue(test)
+
     def test_adding_user_perm(self):
         """Test adding a user w/ permissions"""
-        contenttype = ContentType.objects.get_for_model(amod.User)
-        permission = Permission.objects.create(codename='can_edit', name = 'Can Edit', content_type = contenttype)
+        account = ContentType.objects.get_for_model(amod.User)
+        permission = Permission.objects.create(codename='add_product', name = 'Can Add Product',
+                                               content_type = account)
+        permission.save()
+
         self.u1.user_permissions.add(permission)
         self.u1.save()
 
+        test = self.u1.has_perm('account.add_product')
+        self.assertTrue(test)
+
+
     def test_log_in(self):
         """Test logging in"""
-        c = Client()
-        log_in = c.login(username = self.u1.email, password = self.u1.password)
+        user = authenticate(self, username = self.u1.email, password = self.u1.password)
+        if user is not None:
+            login(user)
+
+        self.assertFalse(self.u1.is_anonymous)
 
     def test_logout(self):
         """Test logout"""
-        c = Client()
-        log_out = c.logout()
+        logout(self.u1)
+        self.assertTrue(self.u1.is_anonymous)
 
     def test_password(self):
         """Testing the password"""
         self.u1.set_password("doublecheck")
         self.u1.check_password("doublecheck")
 
-
     def test_changes(self):
         """Test random changes"""
         self.u1.first_name='Martha'
         self.assertEqual('Martha', self.u1.first_name)
+
         self.u1.last_name='Stewart'
         self.assertEqual('Stewart', self.u1.last_name)
+
         self.u1.address='456 Circle Lane'
         self.assertEqual('456 Circle Lane', self.u1.address)
