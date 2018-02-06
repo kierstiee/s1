ed0820bbb9b9d6061ab94745cef9063311cb448a
kierstiee <kierstiee@gmail.com>
2018-02-06 09:18:50 -0700

    most of signup

---
+++ account/views/login.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django_mako_plus import view_function
+
+
+@view_function
+def process_request(request):
+    # process form
+    if request.method == 'POST':
+        form = TestForm(request.POST)
+        if form.is_valid():
+            # do work of form (e.g., make payment, create user)
+
+            return HttpResponseRedirect('/')
+    else:
+        form = TestForm()
+
+        context = {
+            'form':form,
+        }
+    return request.dmp_render('testform.html', context)
+
+class TestForm(forms.Form):
+    favorite_ice_cream = forms.CharField(label = 'Favorite Ice Cream')
+    renewal_date = forms.DateField(label="Renewal", help_text="Please enter a date between nnow and 4 weeks (default 3).")
--- account/views/signup.py
+++ account/views/signup.py
@@ -1,4 +1,70 @@
 from django import forms
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django_mako_plus import view_function
+from account import models as amod
 
 
-class TestForm(forms.Form):
+@view_function
+def process_request(request):
+    # process form
+    if request.method == 'POST':
+        form = SignupForms(request.POST)
+        if form.is_valid():
+            # once we're here, everything is clean. No more data changes
+
+            # do work of form (e.g., make payment, create user)
+            return HttpResponseRedirect('/')
+    else:
+        form = SignupForms()
+    context = {
+        'form':form,
+    }
+    return request.dmp_render('testform.html', context)
+
+
+class SignupForms(forms.Form):
+    u1 = amod.User()
+    u1.first_name = forms.CharField(label='First Name')
+    u1.last_name = forms.CharField(label='Last Name')
+    u1.email = forms.EmailField(label='Email')
+    u1.password = forms.CharField(widget=forms.PasswordInput(), label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput(), label='Repeat password')
+
+    def clean_password(self):
+        password = self.cleaned_data.get('Password')
+        has_digit = any([c.isdigit() for c in password])
+        if len(password) < 8:
+            raise forms.ValidationError('Password must have at least 8 characters. Please try again.')
+        elif not has_digit:
+            raise forms.ValidationError('Password must have a number. Please try again.')
+        else:
+            return password
+
+    def clean(self):
+        # double password
+        password = self.cleaned_data.get('Password')
+        password2 = self.cleaned_data.get('Repeat Password')
+        email = self.cleaned_data.get('Email')
+        first = self.cleaned_data.get('First Name')
+        last = self.cleaned_data.get('Last Name')
+
+        if not first:
+            raise forms.ValidationError('Please fill all fields')
+        elif not last:
+            raise forms.ValidationError('Please fill all fields')
+        elif password != password2:
+            raise forms.ValidationError('Passwords do not match. Please try again.')
+        return self.cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('Email')
+        if email # not in db:
+            raise forms.ValidationError('This email is already registered. Do you have an account already?')
+
+    def commit(self):
+        try:
+            # create user
+        except Exception as e:
+            print(e)
+        # log in
