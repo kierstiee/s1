b2a9086b686ee517484451cf9111f6fbfc1239d9
kierstiee <kierstiee@gmail.com>
2018-02-07 14:17:37 -0700

    fixing problems

--- formlib-master/formlib/__init__.py
+++
@@ -1,5 +0,0 @@
-# All apps that are DMP-enabled must have this setting in their app-level __init__.py
-DJANGO_MAKO_PLUS = True
-
-# include Formless in the package namespace
-from .form import Formless
