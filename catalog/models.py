69528cf3455e25014bfba75108750dd16b1a65df
kierstiee <kierstiee@gmail.com>
2018-02-13 16:58:08 -0700

    started models

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from django.db.models.fields import DateTimeField
+# from polymorphic import
 
-class Rental(models):
-    create_date = DateTimeField.auto_now_add
-    edit_date = DateTimeField.auto_now
+
+class Category(models):
+    create_date = models.DateTimeField.auto_now_add
+    edit_date = models.DateTimeField.auto_now
+    name = models.TextField(null=True, blank=True)
+
+class Product(models):
+    create_date = models.DateTimeField.auto_now_add
+    edit_date = models.DateTimeField.auto_now
+
+    price = models.DecimalField(null=True, blank=True)
+    status = models.TextField(null=True, blank=True)
