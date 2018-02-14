b71d3fa911e7bfce468241493b08208b65326c79
kierstiee <kierstiee@gmail.com>
2018-02-14 09:15:15 -0700

    more models

--- catalog/models.py
+++ catalog/models.py
@@ -2,14 +2,31 @@ from django.db import models
 # from polymorphic import
 
 
-class Category(models):
+class Category(models.Model):
     create_date = models.DateTimeField.auto_now_add
     edit_date = models.DateTimeField.auto_now
     name = models.TextField(null=True, blank=True)
 
-class Product(models):
+class Product(models.Model):
     create_date = models.DateTimeField.auto_now_add
     edit_date = models.DateTimeField.auto_now
-
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     price = models.DecimalField(null=True, blank=True)
-    status = models.TextField(null=True, blank=True)
+
+    ACTIVE = 'Active'
+    INACTIVE = 'Inactive'
+    CHOICES = (
+        (ACTIVE, 'Active')
+        (INACTIVE, 'Inactive')
+    )
+    status = models.CharField(
+        max_length=8,
+        choices=CHOICES,
+        default=ACTIVE
+    )
+
+class BulkProduct(Product):
+
+class RentalProduct(Product):
+
+class IndividualProduct(Product):
