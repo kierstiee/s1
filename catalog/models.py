f7767e5d2df89d708ee36b6e124dcc6bb81735a2
kierstiee <kierstiee@gmail.com>
2018-02-16 16:39:40 -0700

    adding more, almost getting db to pass data

--- catalog/models.py
+++ catalog/models.py
@@ -24,6 +24,7 @@ class Product(PolymorphicModel):
     edit_date = models.DateTimeField(auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.IntegerField(default=1)
     # image = models.ImageField()
     name = models.TextField()
     description = models.TextField()
@@ -32,17 +33,18 @@ class Product(PolymorphicModel):
 
 
 class BulkProduct(Product):
-
-    quantity = models.IntegerField()
+    TITLE = 'Bulk'
     reorder_trigger = models.IntegerField()
     reorder_quantity = models.IntegerField()
 
 
 class IndividualProduct(Product):
+    TITLE = 'Individual'
     itemID = models.TextField()
 
 
 class RentalProduct(Product):
+    TITLE = 'Rental'
     itemID = models.TextField()
-    retire_date = models.DateTimeField()
-    max_rental = models.IntegerField()
+    retire_date = models.DateTimeField(null=True, blank=True)
+    max_rental_days = models.IntegerField(default=0)
--- manager/views/list_products.py
+++ manager/views/list_products.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
 from catalog import models as cmod
+import psycopg2
+
 
 
 @view_function
 def process_request(request):
-    data = cmod.Products.objects.all()
+    product_list = cmod.Product.objects.all()
+    # count =
     context = {
+        'product_list':product_list
     }
     return request.dmp_render('list_products.html', context)
