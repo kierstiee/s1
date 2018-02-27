684ae82d7e8469f4c681eab3bcc106e0d1e1a055
kierstiee <kierstiee@gmail.com>
2018-02-27 08:01:20 -0700

    merge

--- catalog/views/index.py
+++ catalog/views/index.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
 
+
 @view_function
 def process_request(request):
     utc_time = datetime.utcnow()
