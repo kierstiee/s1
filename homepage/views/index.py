85532bbfcb15cec8656a5f9bc27fa913c0d8dff1
kierstiee <kierstiee@gmail.com>
2018-02-27 19:02:47 -0700

    user experience done

--- homepage/views/index.py
+++ homepage/views/index.py
@@ -6,10 +6,11 @@ from datetime import datetime, timezone
 @view_function
 def process_request(request):
     utc_time = datetime.utcnow()
+    quote = 'Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
     context = {
         # sent to index.html:
-        'welcome': 'Welcome to FOMO! We are dedicated to providing the best service in Utah Valley!',
-
+        'welcome': 'Welcome to FOMO! Our family-run business is dedicated to providing you with the best musical experience in Utah Valley!',
+        'text': quote,
     }
     return request.dmp.render('index.html', context)
 
