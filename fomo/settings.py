71f1b882a10bf6edf3c4a0c5b28ce7efc01a2494
kierstiee <kierstiee@gmail.com>
2018-03-26 17:08:03 -0600

    started Sprint 7

--- fomo/settings.py
+++ fomo/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'catalog.history.LastFiveMiddleware',
+    'account.basicauth.BasicAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'fomo.urls'
