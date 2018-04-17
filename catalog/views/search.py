1a4362f6786ba1dec66851d666534de8b8422463
kierstiee <kierstiee@gmail.com>
2018-04-17 17:05:47 -0600

    One thing more

--- catalog/views/search.py
+++ catalog/views/search.py
@@ -3,9 +3,8 @@ from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
 from catalog import models as cmod
 from account import models as amod
+from decimal import Decimal
 from rest_framework import serializers
-from rest_framework import generics
-from rest_framework.response import Response
 from django.http import JsonResponse
 
 
@@ -13,20 +12,40 @@ from django.http import JsonResponse
 def process_request(request):
     if request.method == 'GET':
         products = cmod.Product.objects.all()
-        products = ProductSerialize(products,many=True)
-        return JsonResponse(products.data, safe=False)
-    context = {
+        cat = request.GET.get('category')
+        name = request.GET.get('name')
+        max_price = request.GET.get('price')
+        if cat != '':
+            cat = cmod.Category.objects.get(name__icontains=cat)
+            products = products.filter(category=cat)
 
+        if name is not None:
+            products = products.filter(name__icontains=name)
+
+        if max_price != '':
+            max_price = Decimal(max_price)
+            products = products.filter(price__lte=max_price)
+
+        products = products.order_by('category','name')
+
+        page_num = request.GET.get('page')
+        max_page = int(page_num) * 6
+        min_page = max_page - 6
+
+        # products = products[max_page-5, max_page]
+
+        products = ProductSerialize(products,many=True)
+        returned = products.data[min_page: max_page]
+        return JsonResponse(returned, safe=False)
+    context = {
     }
     return request.dmp.render('index.html', context)
+
 
 class ProductSerialize(serializers.Serializer):
     category = serializers.CharField()
     name = serializers.CharField()
     price = serializers.DecimalField(decimal_places=2, max_digits=8)
-
-    def create(self, validated_data):
-        return ProductSearch()
 
     def update(self, instance, validated_data):
         instance.category = validated_data.get('category_name', instance.category)
@@ -34,28 +53,3 @@ class ProductSerialize(serializers.Serializer):
         instance.price = validated_data.get('price', instance.price)
         instance.save()
         return instance
-
-
-class ProductSearch(generics.ListAPIView):
-    """This view returns a list of all the products the user is searching for in JSON"""
-    serializer_class = ProductSerialize
-
-    def get_queryset(self):
-        products = cmod.Product.objects.all()
-        category = self.request.query_params.get('category', None)
-        if category is not None:
-            products = products.filter(category__icontains=category)
-
-        name = self.request.query_params.get('name', None)
-        if name is not None:
-            products = products.filter(name__icontains=name)
-
-        max_price = self.request.query_params.get('price',None)
-        if max_price is not None:
-            products = products.filter(price__lte=max_price)
-
-        products.order_by('category','name')
-
-        page_no = self.request.query_params.get('page', 1)
-
-        return Response(products)
